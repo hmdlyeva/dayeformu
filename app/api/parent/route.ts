ed8f07921003ecc9c6cb6e49ebd48b4f658782ea
unknown <bd7fcqzu3@code.edu.az>
2024-02-27 00:50:51 +0400

    form

--- app/api/parent/route.ts
+++ app/api/parent/route.ts
@@ -10,47 +10,47 @@ import {
 } from "firebase/firestore/lite";
 import app from "../firebase";
 
-export async function GET() {
-  const db = getFirestore(app);
-  const citiesCol = collection(db, "Parents");
-  const citySnapshot = await getDocs(citiesCol);
-  const parents = citySnapshot.docs.map((doc) => {
-    return { id: doc.id, ...doc.data() };
-  });
-  try {
-    return NextResponse.json(parents);
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { message: "Some error occured" },
-      { status: 500 }
-    );
-  }
-}
+// export async function GET() {
+//   const db = getFirestore(app);
+//   const citiesCol = collection(db, "Parents");
+//   const citySnapshot = await getDocs(citiesCol);
+//   const parents = citySnapshot.docs.map((doc) => {
+//     return { id: doc.id, ...doc.data() };
+//   });
+//   try {
+//     return NextResponse.json(parents);
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json(
+//       { message: "Some error occured" },
+//       { status: 500 }
+//     );
+//   }
+// }
 
-export async function POST(request: NextRequest, { body }: { body: any }) {
-    try {
-      const db = getFirestore(app);
-      const parentsCol = collection(db, "Parents");
+// export async function POST(request: NextRequest, { body }: { body: any }) {
+//     try {
+//       const db = getFirestore(app);
+//       const parentsCol = collection(db, "Parents");
   
-      const newDocRef = await addDoc(parentsCol, body);
+//       const newDocRef = await addDoc(parentsCol, body);
   
-      const newDocSnapshot = await getDoc(newDocRef);
+//       const newDocSnapshot = await getDoc(newDocRef);
   
-      console.log(newDocSnapshot.data());
+//       console.log(newDocSnapshot.data());
   
-      return NextResponse.json(
-        { id: newDocSnapshot.id, ...newDocSnapshot.data() },
-        { status: 201 }
-      );
-    } catch (error) {
-      console.error(error);
-      return NextResponse.json(
-        { message: "Some error occurred" },
-        { status: 500 }
-      );
-    }
-  }
+//       return NextResponse.json(
+//         { id: newDocSnapshot.id, ...newDocSnapshot.data() },
+//         { status: 201 }
+//       );
+//     } catch (error) {
+//       console.error(error);
+//       return NextResponse.json(
+//         { message: "Some error occurred" },
+//         { status: 500 }
+//       );
+//     }
+//   }
 
   export const submitForm = async (formData: any) => {
     try {
